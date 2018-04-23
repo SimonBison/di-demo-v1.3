21954ccb53288f0823c8edc88c4730c0a537bd60
sbi <szymon.bizon@hicron.com>
2018-04-23 14:02:42 +0200

    Dependency Injection using the Spring Framework.

--- src/main/java/com/gmail/spbisoft/didemo/DiDemoApplication.java
+++ src/main/java/com/gmail/spbisoft/didemo/DiDemoApplication.java
@@ -1,6 +1,9 @@
 package com.gmail.spbisoft.didemo;
 
+import com.gmail.spbisoft.didemo.controllers.ConstructorInjectedController;
 import com.gmail.spbisoft.didemo.controllers.MyController;
+import com.gmail.spbisoft.didemo.controllers.PropertyInjectedController;
+import com.gmail.spbisoft.didemo.controllers.SetterInjectedController;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.ApplicationContext;
@@ -16,5 +19,9 @@ public class DiDemoApplication {
         MyController controller = (MyController) ctx.getBean("myController");
 
         controller.hello();
+
+        System.out.println(ctx.getBean(PropertyInjectedController.class).sayHello());
+        System.out.println(ctx.getBean(SetterInjectedController.class).sayHello());
+        System.out.println(ctx.getBean(ConstructorInjectedController.class).sayHello());
     }
 }
--- src/main/java/com/gmail/spbisoft/didemo/controllers/PropertyInjectedController.java
+++ src/main/java/com/gmail/spbisoft/didemo/controllers/PropertyInjectedController.java
@@ -1,12 +1,16 @@
 package com.gmail.spbisoft.didemo.controllers;
 
 import com.gmail.spbisoft.didemo.services.GreetingServiceImpl;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
 
+@Controller
 public class PropertyInjectedController {
 
+    @Autowired
     public GreetingServiceImpl greetingService;
 
-    String sayHello(){
+    public String sayHello(){
         return greetingService.sayGreeting();
     }
 }
