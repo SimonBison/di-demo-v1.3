f7535556d6f6e2d1974aa55fdb8c644759c160c9
sbi <szymon.bizon@hicron.com>
2018-04-23 11:09:43 +0200

    Initial commit

---
+++ src/main/java/com/gmail/spbisoft/didemo/DiDemoApplication.java
@@ -0,0 +1,20 @@
+package com.gmail.spbisoft.didemo;
+
+import com.gmail.spbisoft.didemo.controllers.MyController;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ApplicationContext;
+
+import static org.springframework.boot.SpringApplication.run;
+
+@SpringBootApplication
+public class DiDemoApplication {
+
+    public static void main(String[] args) {
+        ApplicationContext ctx = SpringApplication.run(DiDemoApplication.class, args);
+
+        MyController controller = (MyController) ctx.getBean("myController");
+
+        controller.hello();
+    }
+}
