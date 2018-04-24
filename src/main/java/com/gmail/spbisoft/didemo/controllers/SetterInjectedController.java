6f61a25c63ed8fc9e0546592478f58db5212600c
sbi <szymon.bizon@hicron.com>
2018-04-24 12:40:29 +0200

    Dependency Injection - using qualifiers.

--- src/main/java/com/gmail/spbisoft/didemo/controllers/SetterInjectedController.java
+++ src/main/java/com/gmail/spbisoft/didemo/controllers/SetterInjectedController.java
@@ -2,6 +2,7 @@ package com.gmail.spbisoft.didemo.controllers;
 
 import com.gmail.spbisoft.didemo.services.GreetingService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.stereotype.Controller;
 
 @Controller
@@ -14,6 +15,7 @@ public class SetterInjectedController {
     }
 
     @Autowired
+    @Qualifier("getterGreetingService")
     public void setGreetingService(GreetingService greetingService){
         this.greetingService = greetingService;
     }
---
+++ src/main/java/com/gmail/spbisoft/didemo/services/GetterGreetingService.java
@@ -0,0 +1,12 @@
+package com.gmail.spbisoft.didemo.services;
+
+import org.springframework.stereotype.Service;
+
+@Service
+public class GetterGreetingService implements GreetingService {
+
+    @Override
+    public String sayGreeting() {
+        return "Hello - I was injected by the getter";
+    }
+}
