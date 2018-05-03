1cf0cbf1c2b938c49fa446482829a594ab155b40
sbi <szymon.bizon@hicron.com>
2018-05-03 13:50:47 +0200

    Added Spring Configuration using Factory Beans.

---
+++ src/main/java/com/gmail/spbisoft/didemo/config/GreetingServiceConfig.java
@@ -0,0 +1,39 @@
+package com.gmail.spbisoft.didemo.config;
+
+import com.gmail.spbisoft.didemo.services.GreetingRepository;
+import com.gmail.spbisoft.didemo.services.GreetingService;
+import com.gmail.spbisoft.didemo.services.GreetingServiceFactory;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Primary;
+import org.springframework.context.annotation.Profile;
+
+@Configuration
+public class GreetingServiceConfig {
+
+    @Bean
+    GreetingServiceFactory greetingServiceFactory(GreetingRepository repository){
+        return new GreetingServiceFactory(repository);
+    }
+
+    @Bean
+    @Primary
+    @Profile({"default", "en"})
+    GreetingService primaryGreetingService(GreetingServiceFactory greetingServiceFactory){
+        return greetingServiceFactory.createGreetingService("en");
+    }
+
+    @Bean
+    @Primary
+    @Profile("es")
+    GreetingService primarySpanishGreetingService(GreetingServiceFactory greetingServiceFactory){
+        return greetingServiceFactory.createGreetingService("es");
+    }
+
+    @Bean
+    @Primary
+    @Profile("de")
+    GreetingService primaryGermanGreetingService(GreetingServiceFactory greetingServiceFactory){
+        return greetingServiceFactory.createGreetingService("de");
+    }
+}
---
+++ src/main/java/com/gmail/spbisoft/didemo/services/GreetingRepository.java
@@ -0,0 +1,10 @@
+package com.gmail.spbisoft.didemo.services;
+
+public interface GreetingRepository {
+
+    String getEnglishGreeting();
+
+    String getSpanishGreeting();
+
+    String getGermanGreeting();
+}
